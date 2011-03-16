1cf32b75dc145a694e7978ea0b4c565730c641e7
Aaron McGowan <me@amcgowan.ca>
2011-03-16 08:43:43 -0400

    Updated the media homepage URL - used for iframe.

--- application/config/application.php
+++ application/config/application.php
@@ -14,5 +14,5 @@ $config = array(
     'site_email' => 'openhalton@gmail.com',
     'search_poly_colors' => array('FFFFC2', 'C2FFC2', 'C2C2FF', 'C26666', 'E085E0', 'C28566', 'FF6666', '66C266', 'FFA366', '85A3A3', '85E0FF', '85FF85', 'FFE0FF', 'E0C285'),
     
-    'media_homepage' => 'http://localhost/clients/msft-redbitdev/wardrep.ca/webapp/media/homepage.html'
+    'media_homepage' => '/media/homepage.html'
 ); /* end array ... */
